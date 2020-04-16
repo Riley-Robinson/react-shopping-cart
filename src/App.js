a849f4877baa2b5e84036744e88ba706dab5734a
Riley-Robinson <rrtoinfinity@gmail.com>
2020-04-16 18:59:28 -0400

    seems like the npm install broke everything in the morning ill staet over on a new copy of the repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
-// Components
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
+
 
 function App() {
 	const [products] = useState(data);
@@ -13,20 +15,24 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
+
+	const removeItem = itemId => {
+		setCart(cart.filter( cartItem => cartItem.id !== itemId));
+	}
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+      		<ProductContext.Provider value={{ products, addItem }}>
+	  			<CartContext.Provider value={{ cart }}>
+        			<Navigation cart={cart} />
+					<Route exact path="/" component={Products} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+					<Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				</CartContext.Provider>
+				</ProductContext.Provider>
 		</div>
 	);
 }
